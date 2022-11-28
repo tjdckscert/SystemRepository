e997192caf9d7246d2196360667a83b60abae6cf
tjdckscert <tjdcks1051@naver.com>
2022-11-28 09:40:29 -0500

    2.1

---
+++ creat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+int main(int argc, char *argv[]){
+	int fd;
+	char *buf = "tjdcks";
+	ssize_t cnt; 
+	int flags = O_WRONLY | O_CREAT | O_TRUNC;
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; 
+	if (argc < 2) {
+	fprintf(stderr, "write new filename\n");
+	exit(1);
+	}
+	if ( (fd = open(argv[1], flags, mode)) == -1 ){
+	perror("open"); 
+	exit(1);
+	}
+	cnt = write(fd, buf, strlen(buf));
+	printf("line = %d\n", cnt);
+	close(fd);
+}
