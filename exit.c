a6843568a8a44fba75ac65afc005e121f6a68f7e
tjdckscert <tjdcks1051@naver.com>
2022-11-29 02:35:58 -0500

    5.2

---
+++ exit.c
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	int exit_status;
+	printf("this process is exit");
+	exit(0);
+}
